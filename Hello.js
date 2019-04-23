4ea3a97e618b035fe753d9bac93486ef3cd4eb49
BillyLiou <billy91281003@gmail.com>
2019-04-23 15:57:32 +0800

    update 04/23 1557

--- Hello.js
+++ Hello.js
@@ -1,2 +1,16 @@
-var sssss = 'Hi~~';
-console.log(sssss);
+
+console.log('first');
+
+async function foo() {
+    try {
+      await setTimeout(()=>{console.log('~~~')},100);
+      await console.log('newResult');
+      await console.log('finalResult');
+      let finalResult1 = 'FINAL!!!';
+      console.log(`Got the final result: ${finalResult1}`);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+  foo();
+  console.log('second');
